4cd23661d922603990540d528fb32b61653b9aec
Alexander Biskop <biskop@stocard.de>
2014-10-28 15:53:04 +0100

    added failing test f validation of missing properties

--- test/index.js
+++ test/index.js
@@ -1,5 +1,6 @@
 
 var request = require('superagent');
+var async = require('async');
 var assert = require('assert');
 
 var port = 2000;
@@ -16,6 +17,29 @@ var offer2 = createOfferData('Schmancy');
 var testOffers = [offer1, offer2];
 
 describe('Offer API', function () {
+  it('should fail to create an offer with missing properties', function(done) {
+    var requiredProperties = [
+      'type',
+      'provider_id',
+      'regions',
+      'title',
+      'valid_from',
+      'valid_until',
+      'delivery_date',
+      'status'
+    ];
+    async.eachSeries(requiredProperties, function(key, cb) {
+      var offerData = createSomeValidOfferData();
+      offerData[key] = undefined;
+      request.post(host + '/offers')
+        .send(offerData)
+        .end(function(err, res) {
+          if (err) return cb(err);
+          assert.equal(res.status, 400);
+          cb();
+        });
+    }, done);
+  });
   it('should create offer1', function (done) {
     request
       .post(host + '/offers')
@@ -109,5 +133,33 @@ function createOfferData(title) {
     title: title,
     provider_id: '100',
     pages: []
+  };
+}
+
+function createSomeValidOfferData() {
+  return {
+    type: 'catalog',
+    provider_id: 'mcdonalds',
+    regions: ['DE', 'US'],
+    title: 'Awesome Title',
+    valid_from: '2014-10-29T00:00:00.000Z',
+    valid_until: '2014-11-30T00:00:00.000Z',
+    delivery_date: '2014-10-28T00:00:00.000Z',
+    status: 'created',
+    pages: [{
+      title: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy',
+      pic_url: 'http://example.com/images/682d6250-5eaf-11e4-8d59-59d9e4019ad5/medium',
+      pic_metadata_url: 'http://example.com/images/682d6250-5eaf-11e4-8d59-59d9e4019ad5',
+      store_url: 'http://foo.bar.baz'
+    }, {
+      pic_url: 'http://example.com/images/682ec1e0-5eaf-11e4-8d59-59d9e4019ad5/medium',
+      pic_metadata_url: 'http://example.com/images/682ec1e0-5eaf-11e4-8d59-59d9e4019ad5',
+      store_url: 'http://foo2.bar.baz/123'
+    }, {
+      title: 'eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. A',
+      pic_url: 'http://example.com/images/68302170-5eaf-11e4-8d59-59d9e4019ad5/medium',
+      pic_metadata_url: 'http://example.com/images/68302170-5eaf-11e4-8d59-59d9e4019ad5',
+    }],
+    is_fullscreen: true
   };
 }
