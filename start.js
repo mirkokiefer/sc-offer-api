0d8597039fe83401b906cecf25b81f571a9cbdde
Mirko Kiefer <mail@mirkokiefer.com>
2014-11-06 15:41:06 +0100

    use s3store in start script

--- start.js
+++ start.js
@@ -1,7 +1,16 @@
 
 var port = process.env.PORT || 5000;
 var hostname = process.env.PUBLIC_HOSTNAME || 'localhost';
-var store = require('s3store-mock')();
+var createStore = require('s3store');
+
+var options = {
+  key: process.env.AWS_KEY,
+  secret: process.env.AWS_SECRET,
+  bucket: process.env.S3_BUCKET,
+  region: process.env.S3_REGION || 'eu-west-1',
+  namespace: process.env.S3_NAMESPACE || 'offers'
+};
+var store = createStore(options);
 
 require('./index').start(hostname, port, store, function () {
   console.log('listening at %s:%s', hostname, port);
